f4775edba25a5af695daa258c4cb6d2b30e07131
MD. Shimul <mdshemul480@gmail.com>
2021-12-25 18:27:10 +0600

    adding contest problem

--- Codeforces Global Round 18/A_Closing_The_Gap.cpp
+++ Codeforces Global Round 18/A_Closing_The_Gap.cpp
@@ -6,32 +6,15 @@ void solve()
 {
     int n;
     cin >> n;
-    vector<int> arr;
-    int min = 1000,
-        max = 0,
-        sum = 0;
+    int sum = 0;
     for (int i = 0; i < n; i++)
     {
         int in;
         cin >> in;
-        if (min > in)
-        {
-            min = in;
-        }
-        if (max < in)
-        {
-            max = in;
-        }
         sum += in;
     }
-
-    int avg = sum / n;
-    if (sum % n == 0)
-    {
-        cout << 0 << endl;
-        return;
-    }
-    cout << (avg - min) << endl;
+    int result = sum % n == 0 ? 0 : 1;
+    cout << result << endl;
 }
 
 int main()
