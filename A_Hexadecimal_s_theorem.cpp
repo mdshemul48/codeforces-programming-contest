564f76ba05d91e0126fdc5b91036f0edbdbfcd10
MD. Shimul <mdshemul480@gmail.com>
2021-12-29 15:39:11 +0600

    adding new problme

---
+++ A_Hexadecimal_s_theorem.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// custom define
+#define ll long long int
+#define fo for
+#define wh while
+#define cout(v) cout << v << endl;
+#define deb(v) cout << #v << "-> " << v << endl;
+
+void exc()
+{
+    ll n, x, y, z;
+    cin >> n;
+    cout << "0 "
+         << "0 " << n << endl;
+}
+
+int32_t main()
+{
+    exc();
+    return 0;
+}
