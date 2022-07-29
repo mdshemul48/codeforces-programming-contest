71ff5d1dc447f88e83d89b47a47a6378fa4b33cf
MD. Shimul <mdshemul480@gmail.com>
2022-07-30 00:33:55 +0600

    adding B_T-Prime

---
+++ B_T-primes.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int a[1000000] = {0};
+    for (int i = 2; i <= 1000000; i++)
+    {
+        if (a[i] == 0)
+        {
+            for (int j = 2; i * j <= 1000000; j++)
+            {
+                a[i * j] = 1;
+            }
+        }
+    }
+    int n;
+    cin >> n;
+    long long int b;
+    long long int sq;
+    while (n--)
+    {
+        cin >> b;
+        sq = sqrt(b);
+        if (b == 1)
+        {
+            cout << "NO" << endl;
+        }
+        else if (sq * sq == b && a[sq] == 0)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+
+    return 0;
+}
