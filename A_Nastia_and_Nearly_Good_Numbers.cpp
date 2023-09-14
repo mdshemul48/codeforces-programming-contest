3d4ef8fffef4663ea13f200e5ca5f25f33556545
MD. Shimul <mdshemul480@gmail.com>
2023-09-15 00:32:34 +0600

    adding new problem

---
+++ A_Nastia_and_Nearly_Good_Numbers.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        long long int a, b;
+        cin >> a >> b;
+
+        if (b == 1)
+        {
+            cout << "NO" << endl;
+            continue;
+        }
+        cout << "YES" << endl;
+        cout << a << " " << a * b << " " << a * (b + 1) << endl;
+    }
+    return 0;
+}
